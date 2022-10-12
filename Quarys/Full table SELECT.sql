5efd4f6b7949ceb367b30cdcd19651455114adb8
Matheus Willians Fernandes Candido <102835123+CandidoFromBrazil@users.noreply.github.com>
2022-10-11 22:53:00 -0300

    Add files via upload

---
+++ Quarys/Full table SELECT.sql
@@ -0,0 +1,17 @@
+SELECT `Continent`, 
+`Region`, 
+`SurfaceArea`, 
+`IndepYear`, 
+`Population`, 
+`LifeExpectancy`, 
+`GNP`, 
+`GNPOld`, 
+`LocalName`, 
+`GovernmentForm`, 
+`HeadOfState`, 
+`Capital`, 
+`Code2`, 
+`Code`, 
+`Name` 
+FROM `world`.`country`;
+
